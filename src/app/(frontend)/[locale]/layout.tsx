66a97699118d541b883a81b1c278db05fc3a0fbb
VeiaG <romapalamar.veiag@gmail.com>
2025-08-04 21:51:02 +0300

    feat: add analytics script to layout for tracking user interactions

--- src/app/(frontend)/[locale]/layout.tsx
+++ src/app/(frontend)/[locale]/layout.tsx
@@ -6,7 +6,7 @@ import Footer from '@/components/Footer'
 import { NextIntlClientProvider, hasLocale } from 'next-intl'
 import { notFound } from 'next/navigation'
 import { routing } from '@/i18n/routing'
-
+import Script from 'next/script'
 export const metadata = {
   description:
     'Welcome to my portfolio! I’m Roman, a web developer passionate about building modern, user-friendly websites and applications. Check out my projects and see what I’ve been working on!',
@@ -30,6 +30,11 @@ export default async function LocaleLayout({
   return (
     <html lang="en">
       {/* <script crossOrigin="anonymous" src="//unpkg.com/react-scan/dist/auto.global.js" /> */}
+      <Script
+        defer
+        src="https://analytics.veiag.dev/script.js"
+        data-website-id="7070bfb1-5627-4b18-80af-e0af4ec20282"
+      ></Script>
       <body className="bg-zinc-950 text-white relative dark flex flex-col">
         <NextIntlClientProvider>
           <Navigation />
